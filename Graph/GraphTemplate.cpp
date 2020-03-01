68bf0a1ece86299659ed01a9ff5f69f4011ebf26
OtsuKotsu <otoxukk.y@gmail.com>
2020-03-01 10:18:49 +0900

    modify graph template a bit

--- Graph/GraphTemplate.cpp
+++ Graph/GraphTemplate.cpp
@@ -5,11 +5,11 @@ using namespace std;
 template<typename T>
 class Edge {
 public:
-    int srch; //searched?探索状態記録「null = -1」
-    int to; //辺の行き先
-    T cost; //辺の重み
-    Edge(int t, T w) : srch(-1), to(t), cost(w) {}
-    Edge(int t, T w, int src) : srch(src), to(t), cost(w) {}
+    int src;  // source 状態記録「null = -1」
+    int to;  // 辺の行き先
+    T cost;  // 辺の重み
+    Edge(int t, T w) : src(-1), to(t), cost(w) {}
+    Edge(int t, T w, int src) : src(src), to(t), cost(w) {}
 };
 template<typename T>
 using W_Graph = std::vector<std::vector<Edge<T>>>;   //重み付きグラフ
