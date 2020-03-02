6c13c408ab160d0db2ea85fd5c47371ae21a31b5
OtsuKotsu <otoxukk.y@gmail.com>
2020-03-02 21:11:09 +0900

    add Non-recursive SegmentTree on DataStructure dir.

---
+++ DataStructure/SegmentTree_NonRecursive.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+/* Segment Tree -----------*/
+template <typename M>
+class SegmentTree {
+    using F = std::function< M(M, M) >;
+private:
+    int n;
+    F func;
+    M M_init;
+    std::vector<M> t;
+public:
+    // constructor
+    SegmentTree(int &n, F func, const M &M_init): n(n), func(func), M_init(M_init) { t.assign(2*n, M_init); }
+    SegmentTree(std::vector<M> &vec, F func, const M &M_init): n(vec.size()), func(func), M_init(M_init) {
+        t.assign(2*vec.size(), M_init);
+        for(int i = 0; i < vec.size(); ++i) t[i+vec.size()] = vec[i];
+    }
+
+    void build() {  // build segment tree
+        for(int i = n-1; i > 1; --i) t[i] = func(t[i<<1], t[i<<1|1]);
+    }
+    void modify(int p, M val) {  // update value at position p
+        for(t[p += n] = val; p > 1; p >>= 1) t[p>>1] = func(t[p], t[p^1]);
+    }
+    M query(int l, int r) {  // reply to query [l, r)
+        M resl = M_init, resr = M_init;
+        for(l += n, r += n; l < r; l >>= 1, r >>= 1) {
+            if(l & 1) resl = func(resl, t[l++]);
+            if(r & 1) resr = func(t[--r], resr);
+        }
+        return func(resl, resr);
+    }
+    M operator[](const int &k) const {return t[k+n];}  // mustn't use to input
+};
+
+
+
+
+/*  verified on ABC 125 - C -> https://atcoder.jp/contests/abc125/tasks/abc125_c  */
+
+
+long long GCD(long long a, long long b){
+    return (b ? GCD(b, a%b) : a);
+}
+
+signed main(){
+    using namespace std;
+
+    // input
+    int n; cin >> n;
+    vector<long long> aa(n);
+    for (int i = 0; i < n; ++i) cin >> aa[i];
+
+    // build
+    SegmentTree<long long> a(aa, [](long long p, long long q){ return GCD(p,q);}, 0LL);
+    a.build();
+
+    // solve
+    long long ans = -1, pp;
+
+    for(int i = 0; i < n; ++i) {
+        if(i==0) pp = a.query(1, n);
+        else if(i==n-1) pp =a.query(0, n-1);
+        else pp = GCD(a.query(0,i), a.query(i+1, n));
+        ans = max(ans, pp);
+    }
+    // output
+    cout << ans << '\n';
+}
