cdb106f402125db96a5eba269b72eed9a28b455b
OtsuKotsu <otoxukk.y@gmail.com>
2020-03-05 21:56:30 +0900

    make lazy segment tree

---
+++ DataStructure/LazySegmentTree_NonRecursive.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+/* Lazy Segment Tree -----------*/
+template <typename M>
+class LazySegmentTree {
+    using F = std::function<M(M, M)>;
+    using G = std::function<M(M, int)>;
+private:
+    int n, h;
+    std::vector<M> t, d;
+    const F func;
+    const G func2;
+    const M M_init;
+    inline void calc(int p, int length) {
+        if(d[p] == M_init) t[p] = func(t[p<<1], t[p<<1|1]);
+        else t[p] = func2(d[p], length);
+    }
+    inline void apply(int p, M value, int length) {
+        t[p] = func2(value, length);
+        if(p < n) d[p] = value;
+    }
+    void _build_(int l, int r) {
+        int length = 2;
+        for(l += n, r += n-1; l > 1; length <<= 1) {
+            l >>= 1; r >>= 1;
+            for(int i = r; i >= l; --i) calc(i, length);
+        }
+    }
+    void push(int l, int r) {
+        int s = h, length = 1 << (h-1);
+        for(l += n, r += n-1; s > 0; --s, length >>= 1) {
+            for(int i = (l >> s); i <= (r >> s); ++i) if(d[i] != M_init) {
+                    apply(i<<1, d[i], length);
+                    apply(i<<1|1, d[i], length);
+                    d[i] = 0;
+                }
+        }
+    }
+public:
+    // constructor
+    LazySegmentTree(int N,F Combiner, G Func_for_segment, M M_init):
+            n(N), func(Combiner), func2(Func_for_segment), M_init(M_init)
+    {
+        h = 0; int m = 1; while(m <= n) {m <<= 1; ++h;}
+        t.assign(2*n, M_init);
+        d.assign(n, M_init);
+    }
+    // set value at position p
+    void set(int &p, const M &value) {t[p+n] = value;}
+    // build
+    void build() { _build_(0,n); }
+
+    void modify(int l, int r, M value) {  // update value in [l, r)
+        if(value == M_init) return;
+        push(l, l+1);
+        push(r-1, r);
+        bool canl = false, canr = false;
+        int length = 1;
+        for(l += n, r += n; l < r; l >>= 1, r >>= 1, length <<= 1) {
+            if(canl) calc(l-1, length);
+            if(canr) calc(r, length);
+            if(l & 1) apply(l++, value, length), canl = true;
+            if(r & 1) apply(--r, value, length), canr = true;
+        }
+        for(--l; r > 0; l >>= 1, r >>= 1, length <<= 1) {
+            if(canl) calc(l, length);
+            if(canr && (!canl || l != r)) calc(r, length);
+        }
+    }
+
+    M query(int l, int r) {  // reply to query in [l, r)
+        push(l, l+1);
+        push(r-1, r);
+        M resl = M_init, resr = M_init;
+        for(l += n, r += n; l < r; l >>= 1, r >>= 1) {
+            if(l & 1) resl = func(resl, t[l++]);
+            if(r & 1) resr = func(t[--r], resr);
+        }
+        return func(resl, resr);
+    }
+    M operator[](int &k) const { return query(k, k+1); }
+};
+
+
+
+/* assignment modifications and range sum queries */
+
+
+signed main(){
+    using namespace std;
+    int _StartTime_ = clock();
+
+    // input and build
+    int n; cin >> n;
+    LazySegmentTree<int> a(n, [](int a,int b){ return a+b;}, [](int p, int k){ return p*k;}, 0);
+    for(int i = 0; i < n; i++) {int p; cin >> p; a.set(i,p);}
+
+    // modify and reply to query
+    for(int i = 0; i < n; i++) {
+        a.modify(i, min(n,i+3) , i);
+        for(int i = 0; i < n; ++i ) cout << a[i] << (i==n-1 ? '\n' : ' ');
+        cout << a.query(0,i+1) << '\n';
+    }
+
+    printf("ExecutionTime : %d /ms\n",1000*(int)((clock()-_StartTime_)/CLOCKS_PER_SEC));
+}
--- DataStructure/SegmentTree_NonRecursive.cpp
+++ DataStructure/SegmentTree_NonRecursive.cpp
@@ -16,10 +16,13 @@ public:
         t.assign(2*vec.size(), M_init);
         for(int i = 0; i < vec.size(); ++i) t[i+vec.size()] = vec[i];
     }
-
-    void build() {  // build segment tree
+    // set value at position p
+    void set(int &p, M value) { t[p+n] = value; }
+    // build
+    void build() {
         for(int i = n-1; i > 1; --i) t[i] = func(t[i<<1], t[i<<1|1]);
     }
+
     void modify(int p, M val) {  // update value at position p
         for(t[p += n] = val; p > 1; p >>= 1) t[p>>1] = func(t[p], t[p^1]);
     }
@@ -31,8 +34,7 @@ public:
         }
         return func(resl, resr);
     }
-    M &operator[](const int &k) &{return t[k+n];}
-    const M &operator[](const int &k) const& {return t[k+n];}
+    M operator[](int &k) const { return query(k, k+1); }
 };
 
 
@@ -40,7 +42,7 @@ public:
 /*  verified on ABC 125 - C -> https://atcoder.jp/contests/abc125/tasks/abc125_c  */
 
 
-long long GCD(long long a, long long b) { return (b ? GCD(b, a%b) : a); }
+int GCD(int a, int b) { return (b ? GCD(b, a%b) : a); }
 
 signed main(){
     using namespace std;
@@ -48,7 +50,7 @@ signed main(){
     // input and build
     int n; cin >> n;
     SegmentTree<int> a(n, [](int p, int q){ return (q ? GCD(q, p%q) : p);}, 0LL);
-    for(int i = 0; i < n; ++i) cin >> a[i];
+    for(int i = 0; i < n; ++i) {int p; cin >> p; a.set(i,p);}
     a.build();
 
     // solve
