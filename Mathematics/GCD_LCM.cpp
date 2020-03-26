edb802ac7dffa741cc2ef7a5d0702d365c3ef43a
OtsuKotsu <otoxukk.y@gmail.com>
2020-03-26 20:05:37 +0900

    modify to avoid overflow.

--- Mathematics/GCD_LCM.cpp
+++ Mathematics/GCD_LCM.cpp
@@ -6,5 +6,6 @@ long long GCD(long long a, long long b){
     return (b ? GCD(b, a%b) : a);
 }
 long long LCM(long long a, long long b) {
-    return a*b/GCD(a,b);
+    long long g = GCD(a,b);
+    return (a/g) * (b/g) * g;  // オーバーフローを避ける
 }
